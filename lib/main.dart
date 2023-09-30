9e7ca3799a4d3f599c560aa8e3da35eaf951c263
PatelVed671 <118421398+PatelVed671@users.noreply.github.com>
2023-09-30 11:45:13 +0530

    Fire Base Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:e_commerce_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
